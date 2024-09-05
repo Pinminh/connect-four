e86537a588d390c6b7c8a9e2a77a1fc99cb406f7
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-09-06 02:32:21 +0700

    feat: add tests for Connect4Game#game_over?

--- spec/connect_4_game_spec.rb
+++ spec/connect_4_game_spec.rb
@@ -1,9 +1,9 @@
 require_relative '../lib/connect_4_game'
 
 describe Connect4Game do
-  subject(:game) { described_class.new(8, 10) }
-
   describe '#input_next_move' do
+    subject(:game) { described_class.new(8, 10) }
+
     context 'when player enters 3 invalid inputs follows 1 valid input' do
       before do
         invalid_input1 = '@a'
@@ -22,6 +22,61 @@ describe Connect4Game do
 
       it 'returns the valid position 4' do
         expect(game.input_next_move).to eq(4)
+      end
+    end
+  end
+
+  describe '#game_over?' do
+    subject(:game_end) { described_class.new(4, 4) }
+
+    context 'when there is a all-colored board' do
+      before do
+        slots = [%i[red red red blue],
+                 %i[red blue red red],
+                 %i[blue red red blue],
+                 %i[red red blue blue]]
+
+        board = game_end.instance_variable_get(:@board)
+        board.instance_variable_set(:@slots, slots)
+        board.instance_variable_set(:@last_slot, { row: 1, col: 1 })
+      end
+
+      it 'returns true since the game can not proceed anymore' do
+        expect(game_end).to be_game_over
+      end
+    end
+
+    context 'when there is a four' do
+      before do
+        slots = [%i[red red red red],
+                 [:blue, :red, nil, nil],
+                 [:blue, :blue, nil, nil],
+                 [:red, :blue, :blue, nil]]
+
+        board = game_end.instance_variable_get(:@board)
+        board.instance_variable_set(:@slots, slots)
+        board.instance_variable_set(:@last_slot, { row: 3, col: 0 })
+      end
+
+      it 'returns true since there is a winning player' do
+        expect(game_end).to be_game_over
+      end
+    end
+
+    context 'when the board is not all-colored and there are no fours' do
+      before do
+        slots = [[:red, :blue, nil, nil],
+                 [:blue, :red, :blue, nil],
+                 [:red, :red, nil, nil],
+                 [:blue, nil, nil, nil]]
+
+        board = game_end.instance_variable_get(:@board)
+        board.instance_variable_set(:@slots, slots)
+        board.instance_variable_set(:@last_slot, { row: 1, col: 2 })
+      end
+
+      it 'returns false since the game is neither tie nor win' do
+        expect(game_end).not_to be_game_over
       end
     end
   end
