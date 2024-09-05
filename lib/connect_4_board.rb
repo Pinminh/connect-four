451a788456a0a20f07b2a8490dfb84dd24eb7337
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-09-05 23:03:23 +0700

    feat: add Connect4Board#put_mark passing tests

--- lib/connect_4_board.rb
+++ lib/connect_4_board.rb
@@ -1,6 +1,6 @@
 class Connect4Board
   attr_reader :next_color
-  
+
   def initialize(row = 6, col = 7)
     @slots = Array.new(col) { Array.new(row) }
     @next_color = :red
@@ -8,5 +8,15 @@ class Connect4Board
 
   def slots
     @slots.clone
+  end
+
+  def put_mark(row, col)
+    return false if col.negative? || col >= @slots.size
+    return false if row.negative? || row >= @slots.first.size
+    return true unless @slots[col][row].nil?
+
+    @slots[col][row] = @next_color
+    @next_color = @next_color == :red ? :blue : :red
+    true
   end
 end
