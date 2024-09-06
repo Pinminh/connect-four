1121ebf6032b66cc45416ca69525d305756c982e
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-09-06 09:10:50 +0700

    feat: add Connect4Game#play_game passing tests

--- lib/connect_4_game.rb
+++ lib/connect_4_game.rb
@@ -20,6 +20,35 @@ class Connect4Game
     @board.all_colored? || @board.four_aligned?
   end
 
+  def winning_result
+    return :tie if @board.all_colored?
+
+    @board.next_color == :red ? :blue : :red
+  end
+
+  def intro_message
+    puts 'The game is all about: drop color coins, ' \
+         'get a row of coins, or a coloumn of coins, ' \
+         'or a diagonal of coins, and we win.'
+  end
+
+  def winning_message(result)
+    puts 'The game is tie!' if result == :tie
+    puts 'The winner is red-player' if result == :red
+    puts 'The winner is blue-player' if result == :blue
+  end
+
+  def play_game
+    intro_message
+    loop do
+      position = input_next_move
+      @board.drop_mark_at_col(position - 1)
+
+      break if game_over?
+    end
+    winning_message(winning_result)
+  end
+
   private
 
   def convert_string_to_position(string)
